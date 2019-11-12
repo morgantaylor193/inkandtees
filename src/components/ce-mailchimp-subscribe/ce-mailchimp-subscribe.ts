5e8fda1527fad7bba5de9ab954321214d07abaa3
morganTaylor193 <morgan.lee@drivetime.com>
2019-11-11 18:33:35 -0700

    wip

--- src/components/ce-mailchimp-subscribe/ce-mailchimp-subscribe.ts
+++ src/components/ce-mailchimp-subscribe/ce-mailchimp-subscribe.ts
@@ -15,24 +15,24 @@ interface MailChimpResponse {
 
 export class CEMailchimpSubscribe {
 	public submitted = false;
-	public mailChimpEndpoint = 'https://gmail.us20.list-manage.com/subscribe/post-json?u=dc689105a468d4f66ddf72f1c&amp;id=29f8943dab&';
-	public error = '';
+  public errorMsg = '';
+  public successMsg = '';
+	private mailChimpEndpoint = 'https://gmail.us20.list-manage.com/subscribe/post-json?u=dc689105a468d4f66ddf72f1c&amp;id=29f8943dab&';
+
   public subscribeForm = this.fb.group({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    name: new FormControl('', [Validators.required])
+    email: new FormControl('', [Validators.required, Validators.email])
   });
 
   constructor( private http: HttpClient,
     private fb: FormBuilder
   ) {};
 
-	onSubmit() {
-    this.error = '';
+	public onSubmit() {
+    this.errorMsg = '';
     console.log(this.subscribeForm, this.subscribeForm.controls['name'], this.subscribeForm.controls['email']);
 		if (this.subscribeForm.status === 'VALID') {
 
 			const params = new HttpParams()
-				.set('FNAME', this.subscribeForm.controls['name'].value)
 				.set('EMAIL', this.subscribeForm.controls['email'].value)
 				.set('b_dc689105a468d4f66ddf72f1c_29f8943dab', '') // hidden input name
 
@@ -42,15 +42,30 @@ export class CEMailchimpSubscribe {
 			this.http.jsonp<MailChimpResponse>(mailChimpUrl, 'c').subscribe(response => {
         console.log(response);
 				if (response.result && response.result !== 'error') {
-					this.submitted = true;
+          this.submitted = true;
+          this.successMsg = response.msg;
+          this.resetForm(5000);
 				}
 				else {
-					this.error = response.msg;
+					this.errorMsg = `Sorry, an error occurred.`;
 				}
 			}, error => {
 				console.error(error);
-				this.error = 'Sorry, an error occurred.';
+				this.errorMsg = 'Sorry, an error occurred.';
 			});
 		}
-	}
+  }
+
+  public resetForm(ms) {
+    setTimeout( () => {
+      this.subscribeForm.reset();
+      this.submitted = false;
+    }, ms);
+
+  }
+  
+  public handleBlur(){
+    this.errorMsg = this.subscribeForm.controls['email'].invalid && this.subscribeForm.controls['email'].dirty ?
+     'Please provide a valid email address.' : '';
+  }
 }
